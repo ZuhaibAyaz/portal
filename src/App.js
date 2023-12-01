679d4c5a13fc7a2170e02e2cb6f558272c7d5783
Zuhaib Ayaz <zuhaibayaz7@gmail.com>
2023-12-01 19:45:32 +0530

    initial

--- src/App.js
+++ src/App.js
@@ -1,23 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import Papa from 'papaparse';
+import {useEffect} from 'react';
+
+import Report from "./Report.csv";
+
+function countOccurrences(csvData) {
+  let activeCount = 0;
+  let drowsyCount = 0;
+  let sleepingCount = 0;
+
+  csvData.forEach((row) => {
+    const status = row.status; 
+
+    switch (status) {
+      case 'Active':
+        activeCount++;
+        break;
+      case 'Drowsy':
+        drowsyCount++;
+        break;
+      case 'Sleeping':
+        sleepingCount++;
+        break;
+    }
+  });
+
+  
+  console.log('Active Count:', activeCount);
+  console.log('Drowsy Count:', drowsyCount);
+  console.log('Sleeping Count:', sleepingCount);
+}
+
 
 function App() {
+ useEffect(()=>{ 
+ 
+  Papa.parse(Report, {
+    header: true,
+    complete: function (result) {
+      
+      const csvData = result.data;
+  
+     
+      countOccurrences(csvData);
+      console.log(result.data);
+    },
+   
+  });
+
+ },[]) 
+ 
+  
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    hello
     </div>
   );
 }
